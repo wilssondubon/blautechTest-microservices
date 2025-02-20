abc07d66bb8bf217bf44c0a06ef0ded2d6da771a
wilssondubon <wilssondubon@gmail.com>
2025-02-20 14:31:29 -0600

    enableeurekaserver

--- eureka-microservice/src/main/java/com/blautech/eureka_microservice/EurekaMicroserviceApplication.java
+++ eureka-microservice/src/main/java/com/blautech/eureka_microservice/EurekaMicroserviceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnabledEurekaServer
+@EnableEurekaServer
 public class EurekaMicroserviceApplication {
 
 	public static void main(String[] args) {
