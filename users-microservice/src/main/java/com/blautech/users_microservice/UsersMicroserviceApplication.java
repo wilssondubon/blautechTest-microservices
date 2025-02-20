716c106fb966500e5260a58650d8a145939690a9
wilssondubon <wilssondubon@gmail.com>
2025-02-20 14:24:05 -0600

    eureka

--- users-microservice/src/main/java/com/blautech/users_microservice/UsersMicroserviceApplication.java
+++ users-microservice/src/main/java/com/blautech/users_microservice/UsersMicroserviceApplication.java
@@ -2,8 +2,11 @@ package com.blautech.users_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UsersMicroserviceApplication {
 
 	public static void main(String[] args) {
