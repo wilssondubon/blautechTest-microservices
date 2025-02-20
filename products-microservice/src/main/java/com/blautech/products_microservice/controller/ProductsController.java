19f89a0f7ebcc1488ae61cbdbd88a5cb47f2e3a6
wilssondubon <wilssondubon@gmail.com>
2025-02-20 14:59:01 -0600

    port eureka

--- products-microservice/src/main/java/com/blautech/products_microservice/controller/ProductsController.java
+++ products-microservice/src/main/java/com/blautech/products_microservice/controller/ProductsController.java
@@ -13,7 +13,6 @@ import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
